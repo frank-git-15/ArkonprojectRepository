7254bac990037e9897ab04e061754b8d85fbab92
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-02-18 20:32:58 -0600

    Se agrego el endpoint para obtener los puntos de acceso mas cercanos a unas coordenadas dadas, tambien se le incorporo la paginacions y se creo la prueba unitaria

--- arkonproject/APIPuntosDeAccesoWifi/schema.py
+++ arkonproject/APIPuntosDeAccesoWifi/schema.py
@@ -2,6 +2,8 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import WifiAccesPoint
 from django.core.paginator import Paginator
+from django.db.models import FloatField
+from django.db.models.expressions import RawSQL
 
 
 #Aqui se define que datos del modelo se pueden consultar
@@ -16,6 +18,7 @@ class Query(graphene.ObjectType):
     puntosDeAccesoWifi = graphene.List(WifiAccesPointType,page=graphene.Int(),pageSize=graphene.Int())
     puntoDeAcceso = graphene.Field(WifiAccesPointType,id= graphene.String(required=True))
     puntosdeAccesoPorColonia = graphene.List(WifiAccesPointType,colonia= graphene.String(required=True),page=graphene.Int(),page_size=graphene.Int())
+    puntosDeAccesoMasCercanos = graphene.List(WifiAccesPointType,latitud = graphene.Float(required=True),longitud = graphene.Float(required=True),page=graphene.Int(),page_size=graphene.Int())
 
     def resolve_puntosDeAccesoWifi(self,info,page,pageSize):
             #Aqui se realiza la consulta a base de datos para eso usamos el modelo PuntoDeAccesoWifi 
@@ -57,7 +60,54 @@ class Query(graphene.ObjectType):
 
         except WifiAccesPoint.DoesNotExist:
             return []
-      
+
+        
+    """
+    Para el calculo de la distancia se uso la formula harversine
+    R = 6371 Radio de la tierra en kilomentros
+    a = sin**2((latitud1Radianes - latitud2Radianes)/2) + cos(latitud1_Radianes) * cos(latitud2) * sin**2( (longitud1_rad - longitud2_rad) / 2)
+    c = 2 * asin(sqrt(a))
+    distancia  = R * c
+
+    Pero en la consulta se uso una formula simpliflicada para mejorarar la legibilidad y la eficiencia eliminando los cuadrados y usando acos directamente
+    es una aproximacion a la formula original
+    """
+
+    def resolve_puntosDeAccesoMasCercanos(self,info,latitud,longitud,page,page_size):
+        
+        try:
+            print("Obteniendo puntos..")
+
+            #Aqui se consultan los puntos wifi con una consulta sql, se le agrego una columna virtual llamada distancia
+            #La cual es el resultado de aplicar la formula harversine simplificada y despues se ordena por esa columna
+            puntoDeAccesoWifi = WifiAccesPoint.objects.annotate(
+                distancia=RawSQL(
+                    """
+                    6371 * acos(
+                        cos(radians(%s)) * cos(radians(latitud)) *
+                        cos(radians(longitud) - radians(%s)) +
+                        sin(radians(%s)) * sin(radians(latitud))
+                    )
+                    """,
+                    (latitud, longitud, latitud),
+                    output_field=FloatField()
+                )
+            ).order_by('distancia')
+            print("Puntos obtenidos")
+
+            print("Ordenando puntos obtenidos")
+            puntos_ordenados = puntoDeAccesoWifi
+            print("Puntos ordenados")
+
+            paginator = Paginator(puntos_ordenados,page_size)
+            current_page = paginator.get_page(page)
+            return current_page
+
+
+            
+        except Exception as e:
+            print(f"Error en resolve_puntosDeAccesoMasCercanos: {e}")
+            return None  # devolver None si algo sale mal
 
 
 schema = graphene.Schema(query=Query)
--- arkonproject/APIPuntosDeAccesoWifi/tests/test_schema.py
+++ arkonproject/APIPuntosDeAccesoWifi/tests/test_schema.py
@@ -116,4 +116,55 @@ def test_puntosdeAccesoPorColonia_endpoint():
 
     #Deben de ser 3 puntos de acceso
     assert cantidad_resultados == 3
+
+
+
+@pytest.mark.django_db
+def test_puntosDeAccesoMasCercanos_endpoint():
+    # Creacion de un cliente GraphQL
+    #Se crean 6 registros de prueba donde se le colocaron los datos de latitud y longitud
+    WifiAccesPoint.objects.create(id="mi id",programa="mi programa",fecha_instalacion="2016-02-25",latitud=19.432813,longitud=-99.116267,colonia="Mi colonia1",alcaldia="mi alcaldia")
+    WifiAccesPoint.objects.create(id="mi id2",programa="mi programa2",fecha_instalacion="2016-02-25",latitud=19.430822,longitud=-99.115276,colonia="Mi colonia2",alcaldia="mi alcaldia2")
+    WifiAccesPoint.objects.create(id="mi id3",programa="mi programa3",fecha_instalacion="2016-02-25",latitud=19.430822,longitud=-99.115276,colonia="Mi colonia3",alcaldia="mi alcaldia3")
+    WifiAccesPoint.objects.create(id="mi id4",programa="mi programa",fecha_instalacion="2016-02-25",latitud=19.437175,longitud=-99.114399,colonia="Mi colonia4",alcaldia="mi alcaldia")
+    WifiAccesPoint.objects.create(id="mi id5",programa="mi programa2",fecha_instalacion="2016-02-25",latitud=19.4384,longitud=-99.1177,colonia="Mi coloni5",alcaldia="mi alcaldia2")
+    WifiAccesPoint.objects.create(id="mi id6",programa="mi programa3",fecha_instalacion="2016-02-25",latitud=19.42711,longitud=-99.1171,colonia="Mi colonia6",alcaldia="mi alcaldia3")
+    # Creacion de un cliente GraphQL
+    client = Client(schema)
+
+    # Definicion de la consulta GraphQL
+    query = '''
+        query {
+            puntosDeAccesoMasCercanos(latitud:19.42700,longitud:-99.1171,page:1,pageSize:2){
+                id
+                colonia
+                latitud
+                longitud
+
+        }
+        }
+    '''
+
+    # Ejecucion de la consulta
+    response = client.execute(query)
+    print("Response ....")
+    print(response)
+
+    # Verificar que la respuesta no contiene errores
+    assert "errors" not in response
+
+    
+    #Se revisa cuantos puntos de acceso hubo en la colonia Mi colonia
+    cantidad_resultados = len(response["data"]["puntosDeAccesoMasCercanos"])
+    print(cantidad_resultados)
+
+    #Deben de ser 3 puntos de acceso
+    assert cantidad_resultados == 2
+
+    #Revisar si el primer resultado tiene el ID "mi id6"
+
+    id_primer_resultado = response["data"]["puntosDeAccesoMasCercanos"][0]["id"]
+    print(id_primer_resultado)
+
+    assert id_primer_resultado == "mi id6"
     
