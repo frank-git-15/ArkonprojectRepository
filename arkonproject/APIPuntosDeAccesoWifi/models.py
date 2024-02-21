f93cdd3c3419a2c62b879759be0d7cf793e98ff9
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-02-21 15:47:22 -0600

    Al atributo fecha instalacion se le agrego que puede ser nulo o estar en blanco

--- arkonproject/APIPuntosDeAccesoWifi/models.py
+++ arkonproject/APIPuntosDeAccesoWifi/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class WifiAccesPoint(models.Model):
     id = models.CharField(max_length=100,primary_key=True)
     programa = models.CharField(max_length=50)
-    fecha_instalacion = models.DateField()
+    fecha_instalacion = models.DateField(null=True, blank=True)
     latitud = models.FloatField()
     longitud = models.FloatField()
     colonia = models.CharField(max_length = 100)
